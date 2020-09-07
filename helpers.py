be7a4a359ca4cfe0466305c2dba2a6d6d43b7ecf
susannakuu <kuusemaa.susanna@gmail.com>
2020-09-07 15:41:21 +0300

    on loodud abifunktsionaalsus - elementide lisamine

---
+++ helpers.py
@@ -0,0 +1,10 @@
+elemendid = []
+
+# lisame ELEMENT juurde
+def lisa_element(nimetus, hind, kogus):
+    elemendid.append({"nimetus": nimetus, "hind": hind, "kogus": kogus})
+
+# lisame ELEMENDID KORRAGA juurde
+def lisa_elemendid(elementide_nimekiri):
+    elemendid = elementide_nimekiri
+    
