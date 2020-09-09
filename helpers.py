d068f0037cedea248d5e5e132f343fb47b95af9d
susannakuu <kuusemaa.susanna@gmail.com>
2020-09-09 09:02:57 +0300

    on testitud abifunktsionaalsus - elementide lisamine

--- helpers.py
+++ helpers.py
@@ -2,9 +2,24 @@ elemendid = []
 
 # lisame ELEMENT juurde
 def lisa_element(nimetus, hind, kogus):
+    global elemendid
     elemendid.append({"nimetus": nimetus, "hind": hind, "kogus": kogus})
 
 # lisame ELEMENDID KORRAGA juurde
 def lisa_elemendid(elementide_nimekiri):
+    global elemendid
     elemendid = elementide_nimekiri
-    
+
+katse_elemendid = [
+    {"nimetus": "leib", "hind": 0.80, "kogus": 20},
+    {"nimetus": "piim", "hind": 0.50, "kogus": 15},
+    {"nimetus": "leib", "hind": 5.60, "kogus": 5},
+]
+
+#testime elementide lisamist
+lisa_elemendid(katse_elemendid)
+print(elemendid)
+
+#testime elemendi lisamist
+lisa_element("kohupiim", 0.60, 15)
+print(elemendid)
