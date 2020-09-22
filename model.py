52a5c9c28df1050e152b4147cf425714049e854c
susannakuu <47834741+susannakuu@users.noreply.github.com>
2020-09-22 20:11:09 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,26 +1,26 @@
 import helpers
-
 class Model:
+    # konstruktor
     def __init__(self, elemendid):
         self.lisa_elemendid(elemendid)
-
+    # elemendite lisamine
     def lisa_elemendid(self, elemendid):
-        return helpers.lisa_elemendid(elemendid)
+        helpers.lisa_elemendid(elemendid)
 
     def lisa_element(self, nimetus, hind, kogus):
-        return helpers.lisa_element(nimetus, hind, kogus)
-
+        helpers.lisa_element(nimetus, hind, kogus)
+    # elementide lugemine
     def loe_elemendid(self):
         return helpers.loe_elemendid()
 
-    def lisa_element(self, nimetus):
+    def loe_element(self, nimetus):
         return helpers.loe_element(nimetus)
-
+    # elemendi uuendamine
     def uuenda_element(self, nimetus, hind, kogus):
-        return helpers.uuenda_element(nimetus, hind, kogus)
-
+        helpers.uuenda_element(nimetus, hind, kogus)
+    # elementide kustutamine
     def kustuta_element(self, nimetus):
-         helpers.kustuta_element(nimetus)
+        helpers.kustuta_element(nimetus)
 
     def kustuta_elemendid(self):
-         helpers.kustuta_elemendid()
+        helpers.kustuta_elemendid()
