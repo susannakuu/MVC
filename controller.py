81fd1e7bf665df4f2b61f060941c672933931c2b
susannakuu <47834741+susannakuu@users.noreply.github.com>
2020-09-22 20:15:43 +0300

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,3 +1,4 @@
+import exceptions
 class Controller:
     def __init__(self, mudel, vaade):
         self.mudel = mudel
@@ -10,8 +11,11 @@ class Controller:
 
     # elemendi kuvamine
     def kuva_element(self, nimetus):
-        element = self.mudel.loe_element(nimetus)
-        self.vaade.kuva_element(nimetus, element)
+        try:
+            element = self.mudel.loe_element(nimetus)
+            self.vaade.kuva_element(nimetus, element)
+        except exceptions.ElemendiEiOle as e:
+            self.vaade.veateade_elementi_ei_ole(nimetus, e)
 
     # elemendi lisamine
     def lisa_element(self, nimetus, hind, kogus):
@@ -19,9 +23,11 @@ class Controller:
             print("Hind peab olema suurem kui 0 EUR")
         elif(kogus <= 0):
             print("Kogus peab olema suurem kui 0")
-        else:
+        try:
             self.mudel.lisa_element(nimetus, hind, kogus)
             self.vaade.lisa_element(nimetus, hind, kogus)
+        except exceptions.ElementJubaOlemas as e:
+            self.vaade.veateade_element_juba_olemas(nimetus, e)
 
     # elemendi uuendamine
     def uuenda_element(self, nimetus, hind, kogus):
