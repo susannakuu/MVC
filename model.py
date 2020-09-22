c4d58f119f703b501d56e1bd8ff633eeee858346
susannakuu <47834741+susannakuu@users.noreply.github.com>
2020-09-22 20:14:59 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,100 +1,26 @@
-import exceptions
+import helpers
+class Model:
+    # konstruktor
+    def __init__(self, elemendid):
+        self.lisa_elemendid(elemendid)
+    # elemendite lisamine
+    def lisa_elemendid(self, elemendid):
+        helpers.lisa_elemendid(elemendid)
 
-elemendid = []
+    def lisa_element(self, nimetus, hind, kogus):
+        helpers.lisa_element(nimetus, hind, kogus)
+    # elementide lugemine
+    def loe_elemendid(self):
+        return helpers.loe_elemendid()
 
-# lisame ELEMENT juurde
-def lisa_element(nimetus, hind, kogus):
-    global elemendid
-    nimetused = []
-    for element in elemendid:
-        nimetused.append(list(element.values())[0])
-    if nimetus in nimetused:
-        raise exceptions.ElementJubaOlemas("Element {} on juba olemas".format(nimetus))
-    else:
-        elemendid.append({"nimetus":nimetus, "hind":hind, "kogus":kogus})
+    def loe_element(self, nimetus):
+        return helpers.loe_element(nimetus)
+    # elemendi uuendamine
+    def uuenda_element(self, nimetus, hind, kogus):
+        helpers.uuenda_element(nimetus, hind, kogus)
+    # elementide kustutamine
+    def kustuta_element(self, nimetus):
+        helpers.kustuta_element(nimetus)
 
-# lisame ELEMENDID KORRAGA juurde
-def lisa_elemendid(elementide_nimekiri):
-    global elemendid
-    elemendid = elementide_nimekiri
-
-# loeme ELEMENDID korraga
-def loe_elemendid():
-    global elemendid
-    loetud_elemendid = []
-    for element in elemendid:
-        loetud_elemendid.append(element)
-    return loetud_elemendid
-
-# loeme KONKREETNE element
-def loe_element(nimetus):
-    global elemendid
-    nimetused = []
-    for element in elemendid:
-            nimetused.append(list(element.values())[0])
-    if nimetus not in nimetused:
-        raise exceptions.ElemendiEiOle("Elementi {} ei eksisteeri".format(nimetus))
-    else:
-       return elemendid[nimetused.index(nimetus)]
-
-# uuendame KONKREETSET elementi
-def uuenda_element(nimetus, hind, kogus):
-    global elemendid
-    nimetused = []
-    for element in elemendid:
-        nimetused.append(list(element.values())[0])
-    if nimetus not in nimetused:
-        print("Elementi {} ei saa uuendada, kuna ta ei eksisteeri".format(nimetus))
-    else:
-        elemendid[nimetused.index(nimetus)] = {"nimetus":nimetus, "hind":hind, "kogus":kogus}
-
-# kustutame KONKREETSET elementi
-def kustuta_element(nimetus):
-    global elemendid
-    nimetused = []
-    for element in elemendid:
-        nimetused.append(list(element.values())[0])
-    if nimetus not in nimetused:
-        print("Elementi {} ei saa kustutada, kuna ta ei eksisteeri".format(nimetus))
-    else:
-        elemendid.remove(elemendid[nimetused.index(nimetus)])
-
-# kustutame KÕIK elemendid
-def kustuta_elemendid():
-    global elemendid
-    elemendid.clear()
-
-def main():
-    # loome katseandmestik
-    katse_elemendid = [
-        {"nimetus": "leib", "hind":0.80, "kogus": 20},
-        {"nimetus": "piim", "hind":0.50, "kogus": 15},
-        {"nimetus": "vein", "hind":5.60, "kogus": 5},
-    ]
-
-    # testime elementide lisamist
-    lisa_elemendid(katse_elemendid)
-
-    # testime üksiku elemendi lisamist
-    lisa_element("kohupiim", 0.90, 15)
-    #lisa_element("vein", 5.60, 5)
-
-    # testime elementide lugemist
-    #print(loe_element("vein"))
-    #print(loe_element("limonaad"))
-
-    # testime elemendi uuendamist
-    uuenda_element("vein", 10.0, 10)
-    print(loe_element("vein"))
-
-    # testime elemendi kustutamist
-    kustuta_element("vein")
-    print(loe_element("vein"))
-
-    # testime elementide kustutamist
-    kustuta_elemendid()
-    print(loe_elemendid())
-
-# käivitame
-if __name__ == "__main__":
-    main()
+    def kustuta_elemendid(self):
+        helpers.kustuta_elemendid()
