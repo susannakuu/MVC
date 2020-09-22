1d21dacbf5d622d92e989f9dba8f0bee64442265
susannakuu <47834741+susannakuu@users.noreply.github.com>
2020-09-22 20:10:22 +0300

    Update mvc_ui.py

--- mvc_ui.py
+++ mvc_ui.py
@@ -3,20 +3,24 @@ from view import View
 from controller import Controller
 
 elemendid = [
-    {"nimetus": "leib", "hind": 0.80, "kogus": 20},
-    {"nimetus": "piim", "hind": 0.50, "kogus": 15},
-    {"nimetus": "leib", "hind": 5.60, "kogus": 5},
+    {"nimetus": "leib", "hind":0.80, "kogus": 20},
+    {"nimetus": "piim", "hind":0.50, "kogus": 15},
+    {"nimetus": "vein", "hind":5.60, "kogus": 5},
 ]
-#testime
-#loeme uus andmestik
+# testimine
+# loome uus andmestik
 pood = Controller(Model(elemendid), View())
-#kõikide elementide kuvamine
+# kõikide elementide kuvamine
 pood.kuva_elemendid()
-#konkreetse elemendi kuvamine
+# konkreetse elemendi kuvamine
 pood.kuva_element("piim")
-#elemendi lisamine
+# elemendi lisamine
+pood.lisa_element("kohuke", 0.60, 15)
+print("lisa veel üks kohuke")
 pood.lisa_element("kohuke", 0.60, 15)
 pood.kuva_element("kohuke")
+# loeme elemendi mis ei eksisteeri
+pood.kuva_element("küpsis")
 # elemendi uuendamine
 pood.uuenda_element("vein", 10.0, 10)
 # elemendi kustutamine
